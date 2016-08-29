ea8b1a5becbafd4083b70e902953dbb3e8b5a56b
Hugo Arganda <harganda@moreycorp.com>
2016-08-29 18:13:05 -0500

    Update builders methods and added element append to builder Create arduBSON Document class Created new example for element append on buidler

---
+++ ardubsonConfig.h
@@ -0,0 +1,11 @@
+#ifndef ardubson_config_h
+#define ardubson_config_h
+
+#define BSON_DOC_SIZE  256 /* Max BSON Document size */
+#define BSON_ELM_SIZE   64 /* Max BSON Element size  */
+#define JSON_MAX_SIZE  128 /* Max JSON string size (BSONObject::jsonString) */
+
+#define BSON_BUFF_SIZE  256 /* Max BSON Document length, increment this value as needed */
+#define BSON_ELEM_SIZE  64 /* Max BSON Element length, increment this value as needed */
+
+#endif /* ardubson_config_h */
