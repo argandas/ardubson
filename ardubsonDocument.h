c436f61a035206f7449d03fe831918eaf679343a
Hugo Arganda <hugo.arganda@gmail.com>
2016-09-11 17:25:47 -0500

    Changed reset method to public

--- ardubsonDocument.h
+++ ardubsonDocument.h
@@ -1,12 +1,12 @@
 /*
-  ardubson.h - Library for the BSON (Binary-JSON) format.
-  Created by Hugo Arganda (argandas), April 6, 2016.
-  Released into the public domain.
-*/
+ ardubson.h - Library for the BSON (Binary-JSON) format.
+ Created by Hugo Arganda (argandas), April 6, 2016.
+ Released into the public domain.
+ */
 /*
-  Check BSON specification at:
-  http://bsonspec.org/spec.html
-*/
+ Check BSON specification at:
+ http://bsonspec.org/spec.html
+ */
 
 #ifndef ardubson_base_h
 #define ardubson_base_h
@@ -18,21 +18,22 @@
 
 class BSONDocument
 {
-  protected:
-    char _doc[BSON_DOC_SIZE];
-    uint32_t _idx;
-    bool _done;
+    protected:
+        char _doc[BSON_DOC_SIZE];
+        uint32_t _idx;
+        bool _done;
 
-    char* index(void);
-    uint8_t appendNum(char value);
-    uint8_t appendBSONElement(BSONElement element);
-    uint8_t appendNum(uint32_t value);
-    uint8_t appendNum(int32_t value);
-    uint8_t appendNum(int64_t value);
-    uint8_t appendStr(const char *data);
+        char* index(void);
+        uint8_t appendNum(char value);
+        uint8_t appendBSONElement(BSONElement element);
+        uint8_t appendNum(uint32_t value);
+        uint8_t appendNum(int32_t value);
+        uint8_t appendNum(int64_t value);
+        uint8_t appendStr(const char *data);
 
-    /* Clear builder buffer */
-    void reset(void);
+    public:
+        /* Clear builder buffer */
+        void reset(void);
 };
 
 #endif /* ardubson_base_h */
