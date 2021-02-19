e3a8f1be8bc0aab52d06b842b8c095f05eddbe41
Hugo Arganda <hugo.arganda@gmail.com>
2021-02-18 23:45:35 -0600

    removed dumpHex function (out of scope)

--- src/ardubsonObject.cpp
+++ src/ardubsonObject.cpp
@@ -244,16 +244,3 @@ char* BSONObject::jsonString(int decimal_places)
     appendJSON("}");
     return (char *)&_jsonStr;
 }
-
-// Private Methods //////////////////////////////////////////////////////////////
-
-void dumpHex(char* data, int len) {
-  for (int i = 0; i < len; i++, data++) {
-    Serial.print("0x");
-    if ((unsigned char)*data <= 0xF) Serial.print("0");
-    Serial.print((unsigned char)*data, HEX);
-    Serial.print(" ");
-    if ((i + 1) % 0x8 == 0) Serial.println();
-  }
-  Serial.println();
-}
