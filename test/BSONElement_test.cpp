7499709559c716d2038598b06a2f9d14ebb63d55
Hugo Arganda <hugo.arganda@gmail.com>
2021-02-21 12:37:16 -0600

    Fixed BE test case

--- test/BSONElement_test.cpp
+++ test/BSONElement_test.cpp
@@ -7,7 +7,13 @@ unittest(BSONElement_int32_t)
     BSONElement be;
     be.Key("hello").Value((int32_t)192);
 
-    assertEqual(10, be.len());
+    /*
+    *   \x10              -> 0x10 = type uint32_t
+    *   hello\x00         -> field name
+    *   \xC0\x00\x00\x00  -> field value (192)
+    */
+   
+    assertEqual(11, be.len());
     assertEqual(BSON_TYPE_INT32, be.getType());
     assertEqual(192, be.getInt());
     assertEqual("hello", be.getKey());
